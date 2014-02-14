5feae7c5e10d3020bd961c6983e9774586a3c5b0
Robert Marchman <robert.l.marchman@dartmouth.edu>
2014-02-13 23:24:58 -0500

    add xcas+xml junk under auth

--- app.js
+++ app.js
@@ -9,17 +9,24 @@ var user = require('./routes/user');
 var http = require('http');
 var path = require('path');
 
+var auth = require('./auth/auth');
+
 var app = express();
 
 // all environments
 app.set('port', process.env.PORT || 3000);
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
-app.use(express.favicon());
 app.use(express.logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded());
 app.use(express.methodOverride());
+
+// auth stuff
+app.use(express.cookieParser('secret goes here'));
+app.use(express.session());
+app.use(auth);
+
 app.use(app.router);
 app.use(require('stylus').middleware(path.join(__dirname, 'public')));
 app.use(express.static(path.join(__dirname, 'public')));
