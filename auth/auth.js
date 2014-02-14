8d4446a10a8d430bb5f3db75a0f77ca65f963b72
Robert Marchman <robert.l.marchman@dartmouth.edu>
2014-02-14 00:18:49 -0500

    basic webauth

---
+++ auth/auth.js
@@ -0,0 +1,34 @@
+
+var CAS = require('./xcas');
+
+var cas = new CAS({
+    base_url: 'https://login.dartmouth.edu/cas/',
+    service: 'http://localhost:3000',
+    version: 2.0
+});
+
+module.exports = function(req, res, next) {
+
+    // should this be mounted here? or explictly in app.js?
+    if (req.url == "/logout") {
+	cas.logout(req, res); // can add logout redirect params here
+    }
+    
+    if (req.session.user) {
+	return next();
+    }
+    
+    cas.authenticate(req, res, function(err, status, username, extended) {
+	if (err) return next(err);
+	
+	// create session for user:
+	req.session.regenerate(function(err) {
+	    if (err) return next(err);
+	    // change this to user object? keyed on db id?
+	    req.session.user = username;
+	    // should we redirect? pass through with next()? (strip ticket?)
+	    res.redirect('/');
+	});
+    });
+}
+
