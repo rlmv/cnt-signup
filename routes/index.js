cc8af2a4e3ffc0e64b957fb7b4fd7a0d35a79b5c
D-Rice <davidrice1992@gmail.com>
2014-02-13 22:22:17 -0500

    context takes fake trips and displays them in default view

--- routes/index.js
+++ routes/index.js
@@ -4,5 +4,21 @@
  */
 
 exports.index = function(req, res){
-  res.render('index', { title: 'Express' });
+    var mytrips = [];
+    var signups = [];
+    var trip1 = { leader : 'bo',
+		  time : 'late at night', 
+		  dest : 'in my bed',
+		  cost : 'a cheap date',
+		  signups : signups}
+    var trip2 = { leader : 'graham',
+		  time : 'early in the morning', 
+		  dest : 'for breakfast',
+		  cost : 'small bills',
+		  signups : signups}
+    mytrips.push(trip1)
+    mytrips.push(trip2)
+    console.log(mytrips)
+    res.render('index', { title: 'Poopface',
+		          trips: mytrips});
 };
