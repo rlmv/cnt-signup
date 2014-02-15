a299a6155cc0adfd51a923c9eab8416925210705
D-Rice <davidrice1992@gmail.com>
2014-02-15 12:40:27 -0500

    we have a page to add trips but not a working post request. we have a working signup page as well

--- app.js
+++ app.js
@@ -6,6 +6,7 @@
 var express = require('express');
 var routes = require('./routes');
 var user = require('./routes/user');
+
 var http = require('http');
 var path = require('path');
 
@@ -19,6 +20,7 @@ app.use(express.favicon());
 app.use(express.logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded());
+app.use(express.bodyParser());
 app.use(express.methodOverride());
 app.use(app.router);
 app.use(require('stylus').middleware(path.join(__dirname, 'public')));
@@ -30,7 +32,10 @@ if ('development' == app.get('env')) {
 }
 
 app.get('/', routes.index);
+app.get('/this_week', routes.this_week);
+app.post('/this_week', routes.trip_signup);
 app.get('/users', user.list);
+app.get('/add_trip', routes.add_trip);
 
 http.createServer(app).listen(app.get('port'), function(){
   console.log('Express server listening on port ' + app.get('port'));
--- routes/index.js
+++ routes/index.js
@@ -4,21 +4,52 @@
  */
 
 exports.index = function(req, res){
+    res.render('index', { title: 'DOC Sign-Up Page'});
+};
+
+exports.trip_signup = function(req, res){
+    console.log(req.body);
+    res.redirect("/this_week");
+};
+
+exports.add_trip = function(req, res){
+    res.render('add_trip', { title: 'Add a Trip to the Site'});
+};
+
+exports.this_week = function(req, res){
     var mytrips = [];
     var signups = [];
-    var trip1 = { leader : 'bo',
-		  time : 'late at night', 
-		  dest : 'in my bed',
-		  cost : 'a cheap date',
+    signups.push("David");
+    var trip1 = { id : 0,
+                  leader : 'bo',
+		  time : 'dawn patrol', 
+		  dest : 'on the dance floor',
+		  cost : 'guarantee of a good time',
 		  signups : signups}
-    var trip2 = { leader : 'graham',
+    var trip2 = { id : 1,
+                  leader : 'graham',
 		  time : 'early in the morning', 
 		  dest : 'for breakfast',
 		  cost : 'small bills',
 		  signups : signups}
+    var trip3 = { id : 2,
+                  leader : 'Amith',
+		  time : 'Middle of the day', 
+		  dest : 'Doctors Office',
+		  cost : 'Medicine',
+		  signups : signups}
+    var trip4 = { id : 3,
+                  leader : 'Jason',
+		  time : 'Evening Hours', 
+		  dest : 'Romantic Valentines Day DInner',
+		  cost : 'True Love',
+		  signups : signups}
+
     mytrips.push(trip1)
     mytrips.push(trip2)
+    mytrips.push(trip3)
+    mytrips.push(trip4)
     console.log(mytrips)
-    res.render('index', { title: 'Poopface',
+    res.render('this_week', { title: 'This Week in Cabin and Trail',
 		          trips: mytrips});
 };
