bc50ad14a0b805e9457177aa118f7c6d13008763
Robert Marchman <robert.l.marchman@dartmouth.edu>
2014-02-18 16:29:41 -0500

    basic config structure for auth

--- auth/auth.js
+++ auth/auth.js
@@ -1,39 +1,42 @@
 
 var CAS = require('./xcas');
 
-var cas = new CAS({
-    base_url: 'https://login.dartmouth.edu/cas/',
-    service: 'http://localhost:3000',
-    version: 2.0
-});
+module.exports = function(options) {
 
-module.exports = function(req, res, next) {
+    var cas = new CAS({
+	base_url: 'https://login.dartmouth.edu/cas/',
+	service: 'http://localhost:3000',
+	version: 2.0
+    });
 
-    // should this be mounted here? or explictly in app.js?
-    if (req.url == "/logout") {
-	cas.logout(req, res); // can add logout redirect params here
-	req.session.destroy(function(err) {
-	    if (err) return next(err);
-	});
-    }
+    return function(req, res, next) {
     
-    if (req.session.auth) {
-	return next();
-    }
-    
-    cas.authenticate(req, res, function(err, status, username, extended) {
-	if (err) return next(err);
+	// should this be mounted here? or explictly in app.js?
+	if (req.url == "/logout") {
+	    cas.logout(req, res); // can add logout redirect params here
+	    req.session.destroy(function(err) {
+		if (err) return next(err);
+	    });
+	}
 	
-	// create session for user:
-	req.session.regenerate(function(err) {
+	if (req.session.auth) {
+	    return next();
+	}
+	
+	cas.authenticate(req, res, function(err, status, username, extended) {
 	    if (err) return next(err);
 	    
-	    // the auth object contains {name, username, netid} fields
-	    req.session.auth = extended.attributes;
+	    // create session for user:
+	    req.session.regenerate(function(err) {
+		if (err) return next(err);
+		
+		// the auth object contains {name, username, netid} fields
+		req.session.auth = extended.attributes;
 
-	    // should we redirect? pass through with next()? (strip ticket?)
-	    return next();
+		// should we redirect? pass through with next()? (strip ticket?)
+		return next();
+	    });
 	});
-    });
+    };
 }
 
