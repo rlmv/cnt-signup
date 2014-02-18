46d3e4f7800f2df40368e9b3676bf8762c3bdc04
Robert Marchman <robert.l.marchman@dartmouth.edu>
2014-02-18 16:50:46 -0500

    make options function args

--- auth/auth.js
+++ auth/auth.js
@@ -1,28 +1,26 @@
 
 var CAS = require('./xcas');
 
-module.exports = function(options) {
-
-    options = options || {};
+module.exports = function(service, logout_url) {
 
     // service is the url of our app
-    if (!options.service) {
+    if (!service) {
 	throw new Error('no service defined');
     }
     
     // logs out of CAS service
-    options.logout_url = options.logout_url || '/logout';
+    logout_url = logout_url || '/logout';
 
     // instantiate CAS object
     var cas = new CAS({
 	base_url: 'https://login.dartmouth.edu/cas/',
-	service: options.service,
+	service: service,
 	version: 2.0
     });
 
     return function(req, res, next) {
 	
-	if (req.url == options.logout_url) {
+	if (req.url == logout_url) {
 	    cas.logout(req, res); // can add logout redirect params here
 	    req.session.destroy(function(err) {
 		if (err) return next(err);
