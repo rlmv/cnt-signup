19dbb302c65a67aac9d541728d271903be8857d3
Robert Marchman <robert.l.marchman@dartmouth.edu>
2014-02-18 21:37:05 -0500

    user auth tweaks

--- auth/user.js
+++ auth/user.js
@@ -13,35 +13,30 @@ module.exports = function(req, res, next) {
     
     var auth = req.session.auth;
     
-    // pull the user from the database here - 
-    // for now we're just saving the username 
-
-    db.User
+    db.User 
 	.find({ where: {netid: auth.netid} })
 	.complete(function(err, user) {
-	    if (err) {
-		return next(err);
-	    } else if (!user) {
-
+	    if (err) return next(err);
+	    else if (!user) {
 		// user not found - add to database
 		db.User
 		    .create({
 			netid: auth.netid,
 			name: auth.name,
 			email: auth.netid + '@dartmouth.edu'
+			// boolean fields set to false by default
 		    })
 		    .complete(function(err, user) {
 			if (err) {
 			    return next(err);
 			} else {
-			    console.log(user);
 			    req.user = user;
 			    next();
 			}
 		    });
 
 	    } else {
-		// attach to request
+		// user found - attach to request
 		req.user = user;
 		next();
 	    }
