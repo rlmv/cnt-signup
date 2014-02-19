b0055a8ffe57eef47899fcea48f4cc93e80a1124
Graham Findlay <graham.b.findlay@gmail.com>
2014-02-19 13:27:44 -0500

    added signup info to add_trip, improved formatting, commented routes, added readme

---
+++ readme.md
@@ -0,0 +1,10 @@
+We're going to want to figure out useful, simple utilities at some point.
+Maybe an admin page where rory can submit freeform database queries?
+Can he do that via heroku? 
+We're going to want a utility to compile the "THIS WEEK" blitz. 
+We're going to want a utility to collect each of a number of selected
+trip statistics during a given time period. 
+
+How should we gracefully handle potentially fluctiating trip capacities?
+Should people simply not get notified that they're actually on the trip
+until the leader/heeler explicitly adds them, giving them permission?
--- routes/routes.js
+++ routes/routes.js
@@ -5,6 +5,13 @@ var db = require('../models');
  */
 
 exports.trip_signup = function(req, res){
+    /*
+     * This should create a trippee signup object and associate it with the trip. 
+     * This should probably be a "waitlisted" association, and people can approve/deny 
+     * the signup later. 
+     * It would be nice to sent them an email with the trip info saying we got their info. 
+     */ 
+
     //instead of printing, change db
     console.log(req.body);
     //give them an error code based on the status of the trip
@@ -17,7 +24,8 @@ exports.trip_signup = function(req, res){
 
 exports.view_add_trip = function(req, res){
     res.render('add_trip', { title: 'Add a Trip to the Site',
-                             user: req.user.name});
+                             user: req.user.name,
+                             netid: req.user.netid});
 };
 
 /*
@@ -25,6 +33,15 @@ exports.view_add_trip = function(req, res){
  */
 
 exports.add_trip = function(req, res){
+    /* this should create a trip object and a signup object and store each in the database
+     * with the proper associations. check the radio button result to see if the person signing up is
+     * a leader or a heeler to choose the right association. 
+     * the data will have to be reformatted as DATETIME for mysql storage, or as 
+     * TIMESTAMP WITH TIME ZONE for PostgreSQL storage. Alternatively, we could just store it as 
+     * a string since it should be a standard format, and maybe moment.js would have a utility 
+     * for calculating trip durations, etc. 
+     */
+
     //instead of just printing, add to the db!
     console.log(req.body);
     //we could route to different pages based on success of db access
@@ -36,6 +53,12 @@ exports.add_trip = function(req, res){
  */
 
 exports.this_week = function(req, res){
+    /* 
+     * This should query the database and for now just fetch all trips in the db,
+     * but only as long as they have a leader signup asociated with them. 
+     * We should probably add a disclaimer that hitting the signup button does not 
+     * guarantee you a spot. You need a confirmation email. 
+     */
 
     //instead of hardcoding these trips in, let's talk to the db!
     var mytrips = [];
