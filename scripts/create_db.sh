f240af64a34c7b5946a447cff6c96d91b9e43a55
Robert Marchman <robert.l.marchman@dartmouth.edu>
2014-02-21 00:32:31 -0500

    rm create db script

--- scripts/create_db.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-DB=cntsignupdb
-USER=root
-
-psql template1 -c "CREATE USER $USER;"
-psql template1 -c " CREATE DATABASE $DB;"
-psql template1 -c " GRANT ALL PRIVILEGES ON DATABASE $DB TO $USER;"
-
