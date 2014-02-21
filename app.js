cbe57d79aba97aff24f9a6975409adb5ec847a47
Robert Marchman <robert.l.marchman@dartmouth.edu>
2014-02-21 10:14:39 -0500

    move static serving before auth middleware

--- app.js
+++ app.js
@@ -20,6 +20,8 @@ app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 app.use(express.favicon(__dirname + '/public/images/favicon.ico'));
 app.use(express.logger('dev'));
+app.use(require('stylus').middleware(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.json());
 app.use(express.urlencoded());
 app.use(express.bodyParser());
@@ -32,8 +34,6 @@ app.use(auth('http:/localhost:3000', '/logout'));
 app.use(user); // save user object on req - should evt. pull from db
 
 app.use(app.router);
-app.use(require('stylus').middleware(path.join(__dirname, 'public')));
-app.use(express.static(path.join(__dirname, 'public')));
 
 // development only
 if ('development' == app.get('env')) {
