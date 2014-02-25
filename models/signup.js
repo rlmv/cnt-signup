9af31068648e99a0f499ba64632010b938d65f6c
Robert Marchman <robert.l.marchman@dartmouth.edu>
2014-02-25 16:23:00 -0500

    rm sequelize models, dependencies

--- models/signup.js
+++
@@ -1,7 +0,0 @@
-//sequelize automatically adds an id : INT field (and createdAt, updatedAt)
-module.exports = function(sequelize, DataTypes) {
-  return sequelize.define("Signup", {
- 	dietary_restrictions: DataTypes.STRING,
- 	comments: DataTypes.TEXT
-  })
-}
