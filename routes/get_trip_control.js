6e288590d4232d5d4147ac4d6e51215abb47d851
Robert Marchman <robert.l.marchman@dartmouth.edu>
2014-03-03 23:43:47 -0500

    queries for trip control

--- routes/get_trip_control.js
+++ routes/get_trip_control.js
@@ -4,22 +4,24 @@ var db = require('../models');
 
 // trip control for leaders - allows basic trippee approval, contact, 
 // etc.
-
 module.exports = function(req, res) {
 
-    db.Trip.findById(req.params.trip_id, function(err, trip) {
-        if (err) throw err;
-        console.log(trip);
+    db.Trip.findById(req.params.trip_id)
+        .populate('leader_signup')
+        .populate('heeler_signup')
+        .populate('waitlist_signups')
+        .populate('approved_signups')
+        .exec(function(err, trip) {
+            if (err) throw err;
 
-    });
+            // the user field is not populated in the signup
+            if (!trip.leader_signup.user == req.user.id) {
+                // change this to display 403 page
+                res.status(403).send('Permission denied');
+            }
 
-    // user does not have permission to edit this trip
-    if (true) {
-        res.status(403).send('Permission denied');
-    }
-    console.log(req.params.trip_id);
-
-    res.render('trip_control', {
-        title: 'Trip Control Panel'
-    });
+            res.render('trip_control', {
+                title: 'Trip Control Panel'
+            });
+        });
 }
