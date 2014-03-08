7f00775e09243a79384d386c30cbf6d85343b4fc
Robert Marchman <robert.l.marchman@dartmouth.edu>
2014-03-08 17:50:51 -0500

    add comments to model instance methods

--- models/index.js
+++ models/index.js
@@ -3,6 +3,17 @@ var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
 var _ = require('underscore');
 
+
+var userSchema = Schema({
+    netid: { type: String, required: true },
+    name: { type: String, required: true },
+    email: { type: String, required: true },
+    is_leader: { type: Boolean, default: false },
+    is_chair: { type: Boolean, default: false },
+    is_admin: { type: Boolean, default: false },
+    is_opo: { type: Boolean, default: false }
+});
+
 var signupSchema = Schema({
     diet: String,
     comments: String,
@@ -42,37 +53,42 @@ tripSchema.virtual('leader_signup').get(function () {
     });
 });
 
+// get heeler signup, if it exists
 tripSchema.virtual('heeler_signup').get(function () {
     return _.find(this.signups, function(signup) {
         return signup.type == 'heeler';
     });
 });
 
+// get all waitlisted signups
 tripSchema.virtual('waitlist_signups').get(function () {
     return _.where(this.signups, { type: 'waitlisted' });
 });
 
+// get all approved signups
 tripSchema.virtual('approved_signups').get(function () {
     return _.where(this.signups, { type: 'approved' });
 })
 
-
-// check if the specified user is a leader
+// is user the leader on this trip?
 tripSchema.methods.isLeader = function(user) {
     var leader_signup = this.leader_signup;
     return leader_signup ? leader_signup.user == user.id : false;
 };
 
+// is user a heeler on this trip?
 tripSchema.methods.isHeeler = function(user) {
     var heeler_signup = this.heeler_signup;
     return heeler_signup ? heeler_signup.user == user.id : false;
 };
 
+// select the signup for user in the trip
 tripSchema.methods.getSignupForUser = function(user) {
     return _.find(this.signups, function(signup) {
         return signup.user == user.id;
     });
 };
+
 // find the signup in the trip with id == id
 tripSchema.methods.getSignupById = function(id) {
     return _.find(this.signups, function(signup) {
@@ -80,17 +96,7 @@ tripSchema.methods.getSignupById = function(id) {
     });
 };
 
-var userSchema = Schema({
-    netid: { type: String, required: true },
-    name: { type: String, required: true },
-    email: { type: String, required: true },
-    is_leader: { type: Boolean, default: false },
-    is_chair: { type: Boolean, default: false },
-    is_admin: { type: Boolean, default: false },
-    is_opo: { type: Boolean, default: false }
-});
     
-
 module.exports = {
     Trip: mongoose.model('Trip', tripSchema),
     User: mongoose.model('User', userSchema)
