3ca7a38fab9879534536cb5b143c0280f511a2ed
Robert Marchman <robert.l.marchman@dartmouth.edu>
2014-04-16 10:44:10 -0400

    record want_to_heel field when heelers claim trip

--- routes/post_claim_trip.js
+++ routes/post_claim_trip.js
@@ -4,21 +4,12 @@ module.exports = function(req, res) {
 
     var body = req.body;
 
-    db.Trip
-	.findOne({ _id: body.trip_id }, function(err, trip) {
-	    if (err) throw err;
+    db.Trip.findOne({ _id: body.trip_id }, function(err, trip) {
 
+    	if (err) throw err;
 	    if (!trip) throw new Error('trip ' + body.trip_id + ' not found!');
 
-	    var type;
-	    if (req.user.is_leader) { // TODO: AND doesnt already have leader
-	    	type = 'leader';
-	    } else {
-	    	type = 'heeler'; // / signup? want-to-heel? what should this be?
-	    }
-
 	    var signup = {
-	    	type: type,	    	
 	    	comments: body.comments,
 	    	diet: body.diet,
 	    	user: req.user,
@@ -28,9 +19,21 @@ module.exports = function(req, res) {
 	    		email: req.user.email
 	    	}
 	    };
+     	// TODO: check that trip doesnt already have leader. This could
+     	// be caused by two leaders loading the page at the same
+     	// time and signing up right after each other. If there is a conflict, 
+     	// show an error message. 
+     	// Actually, should there be validation at the database level? It
+     	// is possible to get a race condition after this validation...
+  	    if (req.user.is_leader) { 
+	    	signup.type = 'leader';
+	    } else {
+	    	signup.type = 'waitlisted'; 
+	    	signup.want_to_heel = true;
+	    }
+
 	    trip.signups.push(signup);
 	    trip.save(function(err, trip) {
-	    	console.log(trip);
 	    	if (err) throw err;
 		    res.redirect('/lead_trip');
 		});
